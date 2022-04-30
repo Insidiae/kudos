faee14e5f11424ed6102e7c266df506d2101da7d
Kobe Ruado <insidiae423@gmail.com>
2022-04-30 22:50:30 +0800

    Add logout route

---
+++ app/routes/logout.ts
@@ -0,0 +1,9 @@
+import { redirect } from "@remix-run/node";
+
+import { logout } from "~/utils/auth.server";
+
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+
+export const action: ActionFunction = async ({ request }) => logout(request);
+
+export const loader: LoaderFunction = async () => redirect("/");
